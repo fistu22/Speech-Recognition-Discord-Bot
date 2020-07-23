49198b8dfc5f5f3ee75de64895dc46250d74526a
Mateusz Bednarek <s18579@pjwstk.edu.pl>
2020-07-23 19:04:24 +0200

    Dodaj pliki projektów.

---
+++ Speech_Recognition_Discord_Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Speech_Recognition_Discord_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+        }
+    }
+}
